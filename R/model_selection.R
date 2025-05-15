b8bc88aa913502b5b296441b5f4566eef7baa3af
ShotaNishijima <nishijimash@gmail.com>
2025-05-15 09:55:08 -0700

    fixed bug in model selection

--- R/model_selection.R
+++ R/model_selection.R
@@ -135,7 +135,7 @@ select_sigma_grid = function(
   tbl_sigma = tibble("stage" = 0, "Age" = NA,"AIC" = samres2$aic,
                      "convergence" = samres2$opt$convergence,
                      "pdHess" = samres2$rep$pdHess,
-                     "maxSE" = max(sqrt(diag(samres2$rep$cov.fixed))))
+                     "maxSE" = max(sqrt(diag(samres2$rep$cov.fixed)),na.rm=TRUE))
   for(i in 1:nrow(grid)) {
     stage_sigma = lapply(1:nrow(grid2), function(j) {
       divide_sigma(samres2,var=as.character(grid2$var[j]),as.numeric(grid2$X[j]))
@@ -151,7 +151,7 @@ select_sigma_grid = function(
         convergence = sapply(stage_sigma,function(X) X$opt$convergence),
         pdHess = sapply(stage_sigma,function(X) X$rep$pdHess),
         aic = sapply(stage_sigma,function(X) X$aic),
-        maxSE = sapply(stage_sigma,function(X) max(sqrt(diag(X$rep$cov.fixed))))
+        maxSE = sapply(stage_sigma,function(X) max(sqrt(diag(X$rep$cov.fixed)),na.rm=TRUE))
       ) %>% mutate(ID = 1:n())
 
       tmp = convergence_aic %>%
@@ -166,9 +166,9 @@ select_sigma_grid = function(
                           tibble("stage" = i, "var" = as.character(grid2$var),"which" = as.numeric(grid2$X),"AIC" = sapply(stage_sigma,function(X) X$aic),
                                  "convergence" = sapply(stage_sigma,function(X) X$opt$convergence),
                                  "pdHess" = sapply(stage_sigma,function(X) X$rep$pdHess),
-                                 "maxSE" = sapply(stage_sigma,function(X) max(sqrt(diag(X$rep$cov.fixed))))))
+                                 "maxSE" = sapply(stage_sigma,function(X) max(sqrt(diag(X$rep$cov.fixed)),na.rm=TRUE))))
 
-    message(paste0("Stage ", i, ": The selected setting is 'var='",as.character(grid2$var[age_stage]), " and 'which'=",as.numeric(grid2$X[age_stage]), " AIC = ", round(min(sapply(stage_sigma,function(X) X$aic)),2)))
+    message(paste0("Stage ", i, ": The selected setting is 'var='",as.character(grid2$var[age_stage]), " and 'which'=",as.numeric(grid2$X[age_stage]), " AIC = ", round(minAIC2,2)))
     grid2 <- grid2[-age_stage,]
     samres2 = stage_sigma[[age_stage]]
     # samres2$rep
@@ -186,20 +186,26 @@ select_sigma_grid = function(
   }
 
   if(isTRUE(check_converge)) {
-    tbl_sigma = tbl_sigma %>% group_by(stage,convergence,pdHess) %>%
-      mutate(model = ifelse(AIC == min(AIC)  & convergence==0 & pdHess == TRUE & maxSE <= SEmax,
-                            "selected",NA))
+    tbl_sigma = tbl_sigma %>%
+      mutate(SE_ok = ifelse(maxSE <= SEmax,TRUE,FALSE)) %>%
+      group_by(stage,convergence,pdHess,SE_ok) %>%
+      mutate(model = ifelse(AIC == min(AIC) & convergence==0 & pdHess == TRUE & SE_ok == TRUE,
+                            "selected",NA)) %>%
+      ungroup() %>%
+      group_by(convergence,pdHess,SE_ok) %>%
+      mutate(model = ifelse(AIC == min(AIC) & convergence==0 & pdHess == TRUE &  SE_ok == TRUE,"best",model)) %>%
+      ungroup() %>% dplyr::select(-SE_ok)
   } else {
     tbl_sigma = tbl_sigma %>% group_by(stage) %>%
-      mutate(model = ifelse(AIC == min(AIC),"selected",NA))
+      mutate(model = ifelse(AIC == min(AIC),"selected",NA)) %>%
+      ungroup() %>%
+      mutate(model = ifelse(AIC == min(AIC),"best",model))
   }
 
   tbl_sigma = tbl_sigma  %>%
-    ungroup() %>%
-    mutate(model = ifelse(AIC == min(AIC),"best",model)) %>%
     dplyr::select(-Age,stage,var,which,convergence,pdHess,AIC,maxSE,model)
   # browser()
-  message(paste0("In the best setting, AIC = ",round(min(tbl_sigma$AIC),2)))
+  message(paste0("In the best setting, AIC = ",round(filter(tbl_sigma,model=="best") %>% pull(AIC),2)))
 
   return(list(tbl_sigma=tbl_sigma,bestres = bestres,reslist=reslist))
 }
