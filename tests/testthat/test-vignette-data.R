130f21729dd0bc7478344c8d2407833f13466833
ichimomo <ichinokawa_momoko09@fra.go.jp>
2026-04-21 14:17:25 +0900

    small change

--- tests/testthat/test-vignette-data.R
+++ tests/testthat/test-vignette-data.R
@@ -100,6 +100,6 @@ expect_equal(res_sam$opt$convergence,0)
 # replace caa as matrix
 res_sam$input$dat$caa <- as.matrix(res_sam$input$dat$caa)
 res_sam2 <- do.call(sam, res_sam$input)
-res_sam$input$silent <- FALSE
+res_sam2$input$silent <- FALSE
 # cannot converge 
 expect_equal(res_sam2$opt$convergence,0)
